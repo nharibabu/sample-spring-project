b6943f57aacae6deae95a9c1f3736a7db9888ca8
Haribabu <nharibabu@dxc.com>
2021-11-08 12:39:47 +0530

    Initial Version

---
+++ src/main/java/com/dxc/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.dxc.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	
+	@GetMapping("/test/greet")
+	public String getGreet() {
+		return "Hi from new laptop!!!";
+	}
+
+}
