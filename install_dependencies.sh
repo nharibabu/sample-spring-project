dfc9c2ab6e0943aaed45f48e07f1cee6743b3ee1
Haribabu <nharibabu@dxc.com>
2021-11-18 12:53:31 +0530

    modified file

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 echo installing java...
 sudo yum install java-1.8.0-amazon-corretto-devel
-echo installing Maven..
-sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d epel-apache-maven.repo
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven
