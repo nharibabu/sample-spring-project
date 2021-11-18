7d294ceaf672a9631a59c23895523953c4d7888c
Haribabu <nharibabu@dxc.com>
2021-11-18 19:48:07 +0530

    modified dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo installing java...
 sudo amazon-linux-extras enable corretto8
-sudo yum install java-1.8.0-amazon-corretto-devel
+sudo yum install -y java-1.8.0-amazon-corretto-devel
 echo installed java...
