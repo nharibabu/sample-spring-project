5207d52901bb383cd6eaa40883b0fc5603f06f95
Haribabu <nharibabu@dxc.com>
2021-11-19 11:13:55 +0530

    modified the files.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo installing java...
-sudo amazon-linux-extras enable corretto8
-sudo yum install -y java-1.8.0-amazon-corretto-devel
-echo installed java...
+if type -p java; then
+    echo found java executable in PATH
+else
+	echo installing java...
+	sudo amazon-linux-extras enable corretto8
+	sudo yum install -y java-1.8.0-amazon-corretto-devel
+	echo installed java...
+fi
